2b4108d345bafb0166f479038642325204c37a7f
boat <supawat.si@kbank.tech>
2018-11-26 17:13:00 +0700

    Commit front-end

--- models/event.go
+++ models/event.go
@@ -8,8 +8,9 @@ type Event struct {
 	ID bson.ObjectId `bson:"_id" json:"id"`
 	Title       string `bson:"title" json:"title"`
 	Description string `bson:"description" json:"description"`
-	Limit       int `bson:"limit" json:"limit"`
 	Avaliable int `bson:"avaliable" json:"avaliable"`
 	Speaker string `bson:"speaker" json:"speaker"`
 	Date        string `bson:"date" json:"date"`
+	Round string`bson:"round" json:"round"`
+	User [] string `bson:"user" json:"user"`
 }
